8f375eb79141e0f130d81a0b73fc7a7097354645
sahumariofragrance <242953512+sahumariofragrance@users.noreply.github.com>
2026-09-25 19:01:28 +0200

    Migrate password recovery and reviews to Next.js

---
+++ src/app/reset-password/page.jsx
@@ -0,0 +1,10 @@
+import ResetPasswordClient from "../../../components-next/ResetPasswordClient";
+
+export const metadata = {
+  title: "Reset Password",
+  robots: { index: false, follow: false },
+};
+
+export default function ResetPasswordPage() {
+  return <ResetPasswordClient />;
+}
