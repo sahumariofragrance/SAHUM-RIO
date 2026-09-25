fdc8f69df78782bb1efc39b1180d9461114e2e2e
sahumariofragrance <242953512+sahumariofragrance@users.noreply.github.com>
2026-09-25 12:27:21 +0200

    Move Next.js app under src

---
+++ src/app/cart/page.jsx
@@ -0,0 +1,10 @@
+import CartPreview from "../../../components-next/CartPreview";
+
+export const metadata = {
+  title: "Cart Preview",
+  robots: { index: false, follow: false },
+};
+
+export default function CartPage() {
+  return <CartPreview />;
+}
